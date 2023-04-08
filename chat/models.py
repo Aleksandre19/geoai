df0eacf1a532fe5622150581188f16cde4b2c450
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2023-04-09 01:53:35 +0200

    Create a Subject, Question and Answer models.

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.conf import settings 
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 
 # Create your models here.
+
+class Question(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    def __str__(self):
+        return self.content[:50] + '...' if len(self.content) > 50 else self.content 
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:50] + '...' if len(self.content) > 50 else self.content
+
+
+class Subject(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    question = GenericRelation(Question)
+
+    def __str__(self):
+        return self.title
+    
+    
+    
+
