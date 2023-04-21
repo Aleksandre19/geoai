b025c9cea0eb80ad039c5da8f5d71eba7afd7aba
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2023-04-21 17:55:19 +0200

    Set the DRF permissions to the DRF views.

--- chat/api/views.py
+++ chat/api/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from chat.api.serializers import TopicSerializer
 from chat.models import Topic
+from chat.api.permissions import IsOwner
 
 
 class TopicList(generics.ListCreateAPIView):
-    queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+    def get_queryset(self):
+        return Topic.objects.filter(user=self.request.user)
 
 
 class TopicDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+    permission_classes = [IsOwner]
