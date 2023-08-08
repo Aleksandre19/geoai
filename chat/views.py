4bc6d7e9395aa548ff6654f19b5faf2654758cf3
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2023-08-08 11:01:50 +0200

    Refactor 'apis_caller' into OOP - chat/views.py.

--- chat/views.py
+++ chat/views.py
@@ -9,12 +9,14 @@ from chat.models import Topic, Answer, Question
 from chat.forms import QuestionForm
 
 from django.utils.text import slugify
-from geoai_translator.views import translate_text
+from geoai_translator.views import translate_text, Translator
+from geoai_openai.views import OpenAI
 
 # from geoai_openai.views import get_openai_response
 
 # from django.utils.safestring import mark_safe
 
+from asgiref.sync import sync_to_async
 from openaiapi.client import openai_response  
 
 from .helpers import *
@@ -22,6 +24,7 @@ from .helpers import *
 
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+import asyncio
 
 logger = logging.getLogger(__name__)
 
@@ -50,7 +53,7 @@ class ChatView(LoginRequiredMixin, ListView):
             return Topic.objects.filter(user = self.request.user)
         
     
-    def get_context_data(self, **kwargs: Any):
+    def get_context_data(self, **kwargs):
         """
         Returns the context data to be used in the template.
         """
@@ -59,6 +62,12 @@ class ChatView(LoginRequiredMixin, ListView):
         titles = Topic.objects.filter(user=self.request.user)
         slug = self.kwargs.get('slug')
         questions = None
+
+        ##### FOR TESTING #####
+        test =  Question.objects.last()
+        ex = exclude_code(test.answer.eng_content)
+        inc = include_back_code(ex['text'])
+        # post_question(test.topic)
         
         if slug:
             # If slug exists, get the Topic object and its related Questions.
@@ -70,7 +79,8 @@ class ChatView(LoginRequiredMixin, ListView):
                 'questions': questions,
                 'question_form': QuestionForm(),
                 'user_id': self.request.user.id,
-                'slug': slug
+                'slug': slug,
+                'test': inc,
         })
         return context
     
@@ -79,11 +89,35 @@ class ChatView(LoginRequiredMixin, ListView):
         """
         Handles the POST requests
         """
+        # Get the slug from the URL.
         slug = self.kwargs.get('slug')
+
+        # Get the Topic object if slug exists.
         topic = get_object_or_404(Topic, slug=slug) if slug else None
-        
-        # Call the function to handle posting a question
-        post_question(request, topic)
+             
+        # Grab the content from the form.
+        form = Form(request, topic).content()
+
+        # Call the APIs (Openai and Google Translate)
+        api = asyncio.run(
+            Apis.call(
+                user=request.user, 
+                question=form['content'],
+                slug=form['slug'], 
+                topic=topic, 
+                openai_model='gpt-3.5-turbo' 
+            )
+        )
+
+        # Insert the content to the database.
+        insert_content(
+            api.user, 
+            api.slug, 
+            api.question,
+            api.geo_eng,
+            api.final_res,
+            api.openAI.answer
+        )
 
         # Redirect based on whether slug exists or not.
         if slug:
@@ -92,54 +126,85 @@ class ChatView(LoginRequiredMixin, ListView):
             return redirect('chat_home')
 
 
-
-def get_all_topics_by_user(request):
-    user_topics = Topic.objects.filter(user=request.user)
-    topics_by_user = {f"topic_{topic.pk}": topic for topic in user_topics}
-    return topics_by_user
-
-
-def get_topic(slug):
-    try:
-        topic = get_object_or_404(Topic, slug=slug)
-    except Http404:
-        topic = None
-    return topic
+class Form:
+    """
+    Handles content grabbing from the question form.
+    """
+    def __init__(self, request, topic):
+        self.topic = topic
+        self.request = request
 
 
-def websocket_chat(user, message, slug):
-    if user.is_authenticated:
-        result = call_apis(user, message, slug)
-        return result
+    # Grab the content from the form.
+    def content(self):
+        form = QuestionForm(self.request.POST, topic=self.topic)
+        if form.is_valid():
+            return form.clean()
+        else: return False
+        
+
+class Apis:
+    """
+    This class takes question, it into english by Google Translate API, 
+    sends it to OpenAI, excludes the code from the response if it exists, 
+    translates the response back to georgian, includes back the excluded code
+    and returns the final response.
+    """
+    def __init__(self, user, question, slug, topic, openai_model):
+        self.user = user # Current user.
+        self.question = question # Original question.
+        self.slug = slug # Slug of the topic.
+        self.topic = topic # Topic object.
+        self.openai_model = openai_model # OpenAI model version.
+        self.geo_eng = None # Question translated from geo to eng.
+        self.eng_geo = None # Response translated from eng to geo.
+        self.openAI = None # OpenAI response.
+        self.without_code = None # Response without code snippet.
+        self.final_res = None # Final response (translated and with code snippet)
+
+    @classmethod
+    async def call(cls, user, question, slug, topic, openai_model):
+        inst = cls(user, question, slug, topic, openai_model)
+        await inst.apis()
+        return inst
 
 
-@login_required
-def post_question(request, topic=None):
-    question_form = QuestionForm(request.POST)  
-    if question_form.is_valid():    
-        # add_topic = topic
-        # Grab content from the form element.
-        question_content =  question_form.cleaned_data['content']
-        if topic:
-            slug = topic.slug
+    async def apis(self):
+        # Translate question from geo to eng.
+        self.geo_eng = await self.translator(self.question, 'ka', 'en-US')
 
-        if not topic:
-            convert_alphabet = convertToEng(question_content[:15])
-            slug = slugify(convert_alphabet)
-            
-        # Call OpenAi API & Google Translate API.
-        call_apis(request.user, question_content, slug)
-    
-        return {
-            "slug": slug
-        }
-    else:
-        # ATTANTION - Needs to be edited
-        return False
+        # Get response from OpenAI.
+        self.openAI = await self.openai()
+
+        # Exclude code (if it is in) from the response.
+        self.without_code = exclude_code(self.openAI.answer)['text']
+
+        # Translate response from eng to geo.
+        self.eng_geo = await self.translator(self.without_code, 'en-US', 'ka')
+
+        # Include code (if it is in) in the response.
+        self.final_res = include_back_code(self.eng_geo.result)
+
+        # Generate slug.
+        self.gen_slug()
+
+    # Google Translate API.
+    async def translator(self, text, from_lan, to_lan):
+        return await Translator.create(text, from_lan, to_lan)
+
+    # OpenAI API.
+    async def openai(self):
+        return await OpenAI.call(self.geo_eng.result, self.slug, self.topic, self.openai_model)
+
+    # Generate slug.
+    def gen_slug(self):
+        if not self.slug:
+            self.slug = slugify(self.geo_eng.result[:20])
+
 
 
 def insert_content(
-        topic,user,slug,geo_question,
+        user,slug,geo_question,
         eng_question,formated_geo_response,
         unformated_geo_response, unformated_eng_response 
         # questionText,
@@ -148,6 +213,8 @@ def insert_content(
         # eng_answer
     ):
     # geo_formated_answer = text_format(geo_unformated_answer)
+    topic = get_topic(slug)
+
     if not topic:
         topic_title = geo_question[:15]
         slug = slug
@@ -180,60 +247,152 @@ def insert_content(
 
 
 
-def call_apis(user, geo_question, slug):
-    # Translate from GEO to ENG.
-    eng_question = translate_text(geo_question, 'ka', 'en-US')
-    if not eng_question:
-            logger.error("Couldn't translate from Geo to Eng.")
-            return
+#########################
 
-    # Call a Openai API.
-    unformated_eng_response = openai_response(eng_question, slug)
-    if not unformated_eng_response:
-        logger.error("Couldn't get the response from the OpenAI.")
-        return
+def get_all_topics_by_user(request):
+    user_topics = Topic.objects.filter(user=request.user)
+    topics_by_user = {f"topic_{topic.pk}": topic for topic in user_topics}
+    return topics_by_user
+
+
+def get_topic(slug):
+    try:
+        topic = get_object_or_404(Topic, slug=slug)
+    except Http404:
+        topic = None
+    return topic
+
+
+def websocket_chat(user, message, slug):
+    if user.is_authenticated:
+        result = api_caller(user, message, slug)
+        return result
+
+
+@login_required
+def post_question(request, topic=None):
+    form = QuestionForm(request.POST, topic=topic)
+    if form.is_valid():
+        form_cont = form.clean();
+        print('===================================', form_cont['content'])
+        api_caller(request.user, form_cont['content'], form_cont['slug'])
+    else: return False
+
+    #     # add_topic = topic
+    #     # Grab content from the form element.
+    #     question_content =  question_form.cleaned_data['content']
+    #     if topic:
+    #         slug = topic.slug
+
+    #     if not topic:
+    #         convert_alphabet = convertToEng(question_content[:15])
+    #         slug = slugify(convert_alphabet)
+            
+    #     # Call OpenAi API & Google Translate API.
+    #     api_caller(request.user, question_content, slug)
     
-    response_exclude_snippet = exclude_code(unformated_eng_response)['text']
+    #     return {
+    #         "slug": slug
+    #     }
+    # else:
+    #     # ATTANTION - Needs to be edited
+    #     return False
 
-    # Translate from ENG to GEO.
-    geo_response_without_snippet = translate_text(
-        response_exclude_snippet,
-        'en-US', 'ka')
+
+
+
+
+
     
-    if not geo_response_without_snippet:
-        logger.error("Couldn't translate from Eng to Geo.")
-        return
+
+
+
+# async def api_caller(user, geo_question, slug, topic):
+#     print('QUESTION =========================================', geo_question)
+#     # Translate from GEO to ENG.
+#     # eng_question = translate_text(geo_question, 'ka', 'en-US')
+#     eng_question = await Translator.create(geo_question, 'ka', 'en-US')
+#     print('TEXT =========================================')
+#     print(eng_question.text)
+#     print('TEXT =========================================')
+#     if not eng_question:
+#             print('=========================================')
+#             logger.error("Couldn't translate from Geo to Eng.")
+#             print('=========================================')
+#             return
+
+#     # Call a Openai API.
+#     print('BEGINNING OF OPENAPI =========================================')
+#     api_model = 'gpt-3.5-turbo'
+#     unformated_eng_response = await OpenAI.call(eng_question.text, slug, topic, api_model)
+#     print('RESPONSE')
+#     pprint.pprint(unformated_eng_response.answer)
+#     # unformated_eng_response = openai_response(eng_question.text, slug)
+#     print('END OF OPENAPI =========================================')
+#     if not unformated_eng_response:
+#         logger.error("Couldn't get the response from the OpenAI.")
+#         return
     
-    response_return_snippet = include_back_code(geo_response_without_snippet)
-    unformated_geo_response = response_return_snippet
+#     response_exclude_snippet = exclude_code(unformated_eng_response.answer)['text']
 
-    formated_geo_response = response_return_snippet
-    # formated_geo_response = text_format(response_return_snippet)
-    # Reassembly the translated text and the extracted content.
+#     # Translate from ENG to GEO.
+#     # geo_response_without_snippet = translate_text(
+#     #     response_exclude_snippet,
+#     #     'en-US', 'ka')
+#     geo_response_without_snippet = await Translator.create(
+#         response_exclude_snippet,
+#         'en-US', 'ka')
+    
+#     print('GEO TO ENG TRANSLATED TEXT =========================================')
+#     pprint.pprint(geo_response_without_snippet.text)
+#     print('END GEO TO ENG TRANSLATED TEXT =========================================')
+    
+#     if not geo_response_without_snippet:
+#         logger.error("Couldn't translate from Eng to Geo.")
+#         return
+    
+#     response_return_snippet = include_back_code(geo_response_without_snippet.text)
+#     unformated_geo_response = response_return_snippet
 
-    if not slug:
-        slug = slugify(eng_question[:20])
-        
-    topic = get_topic(slug)
-    result = insert_content(
-        topic, user, slug, 
-        geo_question,
-        eng_question,
-        formated_geo_response,
-        unformated_geo_response,
-        unformated_eng_response
-    )
+#     formated_geo_response = response_return_snippet
+#     # formated_geo_response = text_format(response_return_snippet)
+#     # Reassembly the translated text and the extracted content.
+#     print('FORMATED RESPONSE =========================================')
+#     pprint.pprint(response_return_snippet)
+#     print('END FORMATED RESPONSE =========================================')
+#     if not slug:
+#         slug = slugify(eng_question[:20])
 
-    return {
-        # 'question': {
-        #     'geo': message,
-        #     'eng': question_geo_to_eng,
-        # },
-        'response': {
-            'geo': formated_geo_response,
-            'eng': unformated_eng_response,
-            'slug': slug,
-            'topic_id': result['topic_id'],
-        }
-    }
+
+#     return {
+#         'user': user,
+#         'slug': slug,
+#         'geo_question': geo_question,
+#         'eng_question': eng_question,
+#         'formated_geo_response': formated_geo_response,
+#         'unformated_geo_response': unformated_geo_response,
+#         'unformated_eng_response': unformated_eng_response.answer,
+#     }
+
+#     result = insert_content(
+#         topic, user, slug, 
+#         geo_question,
+#         eng_question,
+#         formated_geo_response,
+#         unformated_geo_response,
+#         unformated_eng_response
+#     )
+
+#     return {
+#         # 'question': {
+#         #     'geo': message,
+#         #     'eng': question_geo_to_eng,
+#         # },
+#         'response': {
+#             'geo': formated_geo_response,
+#             'eng': unformated_eng_response,
+#             'slug': slug,
+#             'topic_id': result['topic_id'],
+#         }
+#     }
 
