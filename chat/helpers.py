682078357ecf3e0d86624d0090543138a74dee6c
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2023-08-12 22:28:00 +0200

    Refactor 'chat/helpers.py' into OOP.

--- chat/helpers.py
+++ chat/helpers.py
@@ -27,6 +27,127 @@ wrappes rest of the text with <p> elements,
 attaches comment dict(grabed from the code snippet) for translation
 and returns the text.
 """
+class MainHelper:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self):
+        if not self.__dict__:
+            self.cur_prog_lang = {} # Code snippet's programming language
+            self.saved_org_comments = {} # Save a original comments from the code snippet
+            self.attached_comment_name = {} # Save a attached comment
+            self.tokinized_snippet = {} # Save a code snippet
+            self.excluded_words = [] # Save a code snippet
+
+    def generate_place_holder(self, length=6):
+        return str(uuid4().int)[-length:]
+    
+
+class ExcludeCode(MainHelper):
+    def __init__(self, text):
+        super().__init__()
+        self.text = text
+        self.code = None    
+
+    @classmethod
+    def to(cls, text):
+        instance = cls(text)
+        return instance.process()
+    
+    def process(self):
+        plain_text = ExtractCode.to(self.text)
+        return plain_text
+    
+
+
+class ExtractCode(MainHelper):
+    def __init__(self, text):
+        super().__init__()
+        self.text = text
+        
+    @classmethod
+    def to(cls, text):
+        instance = cls(text)
+        return instance.extract()
+
+    def extract(self):
+        plain_text = re.sub(r'```(.*?)```', self.re_func , self.text, flags=re.DOTALL)
+        return plain_text
+
+    def re_func(self, snippet):
+        code_snippet = snippet.group(1) # Grab a code snippet from Regex match.
+        prog_lang_name = code_snippet.splitlines()[0] # Grab a programming language name.
+
+        # Save programming language name.
+        self.cur_prog_lang['name'] = prog_lang_name
+
+        # Remove a language name from the beginning of the snippet.
+        if code_snippet.startswith(self.cur_prog_lang['name']):
+            code_snippet = code_snippet.replace(self.cur_prog_lang['name'], '', 1)
+
+        # Tokenize the code snippet.
+        tokenized = TokenizeText.to(code_snippet)
+
+        # Replace the comments with the place holders.
+        extracted = self.replace_comment_with_plcholder(tokenized) 
+
+        return extracted
+
+    # Replace the comments with the place holders and save them.
+    def replace_comment_with_plcholder(self, tokenized):
+        tokenized_with_placeholder = []
+        for token_type, value in tokenized:
+            if 'Comment' in str(token_type):
+                place_holder = self.generate_place_holder(lenght=6)
+                self.saved_org_comments[place_holder] = value
+                value = place_holder
+            tokenized_with_placeholder.append((token_type, value))
+        return tokenized_with_placeholder
+    
+ 
+class TokenizeText(MainHelper):
+    def __init__(self, code):
+        super().__init__()
+        self.code = code
+        
+
+    @classmethod
+    def to(cls, code):
+        instance = cls(code)
+        return instance.tokenize()
+
+    def tokenize(self):
+        print('=========== self.cur_prog_lang in Tokinaizer')
+        pprint.pprint(self.cur_prog_lang)
+        lexer = self.identify_lexer(self.cur_prog_lang['name'])
+        tokens = list(lex(self.code, lexer))
+        return tokens  
+
+    def identify_lexer(self, prog_lang):
+        lexer_instance = self.import_lexer_module(prog_lang)()
+        if lexer_instance is None:
+            return guess_lexer(prog_lang)
+        return lexer_instance
+    
+    # Import only lexer for the current programming language.
+    def import_lexer_module(self, prog_lang):
+        class_path = Lexers.lexer.get(prog_lang, None)
+    
+        if class_path:
+            module_name, class_name = class_path.rsplit('.', 1)
+            module = importlib.import_module(module_name)
+            if not module:
+                return lambda: None
+            lexer_class = getattr(module, class_name)
+            return lexer_class
+        return lambda: None
+
+
+
 attached_comment_name = {} # Save a attached comment
 tokenized_snippet = {}
 excluded_words = []
@@ -60,6 +181,7 @@ def set_placeholder(snippet):
     return place_holder
 
 
+
 """
 This function receives a code snippet as a parameter,
 tokenizes it, grabs the comments, and stores them into a dictionary.
@@ -85,6 +207,65 @@ def exclud_comments(snippet):
 
 
 """
+This function generates a unique place holder.
+"""
+def generate_place_holder():
+    return str(uuid4().int)[-6:]
+
+"""
+This function takes text without code snippet,
+findes backsticks (`) into text and
+wrappes them with <span> element.
+"""
+def wrap_backsticks(text):
+    excluded_words.append(text.group(1))
+    return f'<span class="bckstk-wrapper">{text.group(1)}</span>'
+
+
+"""
+This function takes a text, breakes it into lines
+and wrappes each line with the <p> element.
+"""
+def wrap_with_p(text):
+    splited_value = text.splitlines()
+    wrapped_by_p = []
+    for item in splited_value:
+        if item and item not in tokenized_snippet:
+            wrapped_by_p.append(f'<p>{item}</p>')
+        else:
+            wrapped_by_p.append(f' {item} ')
+            
+    reconstructed_value = ''
+    for item in wrapped_by_p:
+        reconstructed_value += item
+    return reconstructed_value
+
+
+# """
+# This function receives a code snippet as a parameter,
+# tokenizes it, grabs the comments, and stores them into a dictionary.
+# """
+# saved_comments = {}
+# current_pr_lang = {}
+# def exclud_comments(snippet):
+#     # Store programming language name.
+#     current_pr_lang['name'] = snippet.splitlines()[0] 
+#     # Remove a language name from the beginning of the snippet.
+#     if snippet.startswith(current_pr_lang['name']):
+#         snippet = snippet.replace(current_pr_lang['name'], '', 1)
+        
+#     tokenized = tokeneaze(snippet)
+#     tokenized_with_placeholder = []
+#     for token_type, value in tokenized:
+#         if 'Comment' in str(token_type):
+#             place_holder = generate_place_holder()
+#             saved_comments[place_holder] = value
+#             value = place_holder
+#         tokenized_with_placeholder.append((token_type, value))
+#     return tokenized_with_placeholder
+
+
+"""
 This function uses the Pygments lex method to tokenize the text.
 """
 def tokeneaze(snippet):
@@ -105,23 +286,23 @@ def identify_lexer(pr_lang):
     return lexer_instance
 
 
-"""
-This function takes a text, breakes it into lines
-and wrappes each line with the <p> element.
-"""
-def wrap_with_p(text):
-    splited_value = text.splitlines()
-    wrapped_by_p = []
-    for item in splited_value:
-        if item and item not in tokenized_snippet:
-            wrapped_by_p.append(f'<p>{item}</p>')
-        else:
-            wrapped_by_p.append(f' {item} ')
+# """
+# This function takes a text, breakes it into lines
+# and wrappes each line with the <p> element.
+# """
+# def wrap_with_p(text):
+#     splited_value = text.splitlines()
+#     wrapped_by_p = []
+#     for item in splited_value:
+#         if item and item not in tokenized_snippet:
+#             wrapped_by_p.append(f'<p>{item}</p>')
+#         else:
+#             wrapped_by_p.append(f' {item} ')
             
-    reconstructed_value = ''
-    for item in wrapped_by_p:
-        reconstructed_value += item
-    return reconstructed_value
+#     reconstructed_value = ''
+#     for item in wrapped_by_p:
+#         reconstructed_value += item
+#     return reconstructed_value
 
 
 """
@@ -174,13 +355,6 @@ def test_laxer_import():
 #     # this needs to me removed because snippet is already lexered in the begonnig. 
 #     lexer = guess_lexer(snippet)
 #     return highlight(snippet, lexer, formatter)
-
-
-"""
-This function generates a unique place holder.
-"""
-def generate_place_holder():
-    return str(uuid4().int)[-6:]
 
 
 """
@@ -252,14 +426,14 @@ def include_back_code(text):
     return text
 
 
-"""
-This function takes text without code snippet,
-findes backsticks (`) into text and
-wrappes them with <span> element.
-"""
-def wrap_backsticks(text):
-    excluded_words.append(text.group(1))
-    return f'<span class="bckstk-wrapper">{text.group(1)}</span>'
+# """
+# This function takes text without code snippet,
+# findes backsticks (`) into text and
+# wrappes them with <span> element.
+# """
+# def wrap_backsticks(text):
+#     excluded_words.append(text.group(1))
+#     return f'<span class="bckstk-wrapper">{text.group(1)}</span>'
 
 
 """
@@ -300,3 +474,6 @@ def add_to_cache(topic):
         get_cached_topics = {}
     get_cached_topics[topic_cache_key] = topic
     cache.set("topics_by_user", get_cached_topics, 1)
+
+
+
