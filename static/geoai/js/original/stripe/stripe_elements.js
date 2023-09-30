22e450ead01182701ba94f3fe9623edf2dd298a9
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2023-09-30 13:28:24 +0200

    Add a stripe card style & a error checker function.

--- static/geoai/js/original/stripe/stripe_elements.js
+++ static/geoai/js/original/stripe/stripe_elements.js
@@ -6,5 +6,45 @@ let stripe_client_secret_key = document.getElementById('id_stripe_client_secret_
 
 const stripe = Stripe(stripe_public_key);
 const elements = stripe.elements();
-const card = elements.create('card');
+
+const style = {
+   style: {
+    base: {
+      iconColor: '#c4f0ff',
+      color: '#fff',
+      fontWeight: '500',
+      fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+      fontSize: '16px',
+      fontSmoothing: 'antialiased',
+      ':-webkit-autofill': {
+        color: '#fce883',
+      },
+      '::placeholder': {
+        color: '#87BBFD',
+      },
+    },
+    invalid: {
+      iconColor: '#FFC7EE',
+      color: '#FFC7EE',
+    },
+  },
+};
+
+const card = elements.create('card', { style: style });
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        errorDiv.innerHTML = html;
+    } else {
+        errorDiv.textContent = '';
+    }
+});
