d0468e18ec0838a7c0e24509388bb526b6b25d40
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2023-10-03 12:42:45 +0200

    Rewrite the payment flow - finish.

--- geoai_payments/views.py
+++ geoai_payments/views.py
@@ -29,13 +29,18 @@ class Checkout(ListView):
         # Payment form.
         payment_form = PaymentForm()
 
+        # Get amount from get request.
+        amount = self.kwargs.get('amount')
+        # Convert to Stripe format.
+        stripe_amount = int(amount) * 100
+
         if not settings.STRIPE_PUBLIC_KEY:
             messages.warning(self.request, 'ვერ მოიძებნა Stripe - ის საჯარო გასარები.')
 
         # Stripe 
         stripe.api_key = settings.STRIPE_SECRET_KEY
         intent = stripe.PaymentIntent.create(
-            amount=100,
+            amount=stripe_amount,
             currency = settings.STRIPE_CURRENCY
         )
 
@@ -43,11 +48,13 @@ class Checkout(ListView):
             'welcome': 'GeoAI - ის ჟეტონების შეძენის გვერდი.',
             'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
             'stripe_client_secret_key': intent.client_secret,
-            'payment_form': payment_form
+            'payment_form': payment_form,
+            'amount': amount
         })
 
         return context
     
 
     def post(self, request, *args, **kwargs):
-        return redirect('checkout')
+        amount = self.kwargs.get('amount')
+        return redirect('checkout', amount=amount)
