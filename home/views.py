cb91ae92e31373e77a73b486376a0bb041e75219
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2023-10-19 14:18:16 +0200

    Select a default language in the language select element.

--- home/views.py
+++ home/views.py
@@ -4,6 +4,24 @@ from allauth.account.views import LoginView
 
 class CustomLoginView(LoginView):
     template_name = 'account/login.html'
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+
+        # Grab user language from the cookies.
+        lang = self.request.COOKIES.get('django_language', None)
+
+        # Map language code to the it's name.
+        languages = {
+            'ka': 'Georgian',
+            'en': 'English',
+        }
+        
+        context.update({
+            'language': languages.get(lang, 'English'),
+        })
+
+        return context
     
 # def home(request):
 #     return render(request, 'home/index.html')
