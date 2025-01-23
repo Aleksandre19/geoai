e35186922f8956ecf15ecc6c54915c6ad2b93119
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2025-01-23 13:02:56 +0100

    Remove API keys

--- geoai/settings.py
+++ geoai/settings.py
@@ -34,13 +34,10 @@ class Dev(Configuration):
 
     AUTH_USER_MODEL = 'geoai_auth.User'
 
-    OPENAI_API_KEY = values.Value(environ_name='OPENAI_API_KEY', environ_prefix='')
-    # OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-
-    STRIPE_PUBLIC_KEY = 'pk_test_51NvSdtEjJBdnvl05Vd8eiczbKqMWlZxvjROjnimkAJnZBs4cnakIOFdEDNbt9yIe5xucoJdZFBKpgMF7GozZlqZE00IjJ0SiZi'
-    STRIPE_SECRET_KEY = values.Value(environ_name='STRIPE_SECRET_KEY', environ_prefix='')
-    # STRIPE_WH_SECRET = values.Value(environ_name='STRIPE_WH_SECRET', environ_prefix='')
-    STRIPE_WH_SECRET = 'whsec_ba611445a0fd9e822ae8b50b810a0f725f7de0e77f44c7c820351f95dc7e4a64'
+    OPENAI_API_KEY = ''
+    STRIPE_PUBLIC_KEY = ''
+    STRIPE_SECRET_KEY = ''
+    STRIPE_WH_SECRET = ''
     STRIPE_CURRENCY = 'gel'
 
     OPENAI_DEFAULT_MODEL = 'gpt-3.5-turbo'
